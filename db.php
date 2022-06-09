cc68ef179e8f547f3dd685f5c8db85b2cffa6494
mackliu <macklun@gmail.comm>
2022-06-09 22:21:16 +0800

    自訂函式練習範本

---
+++ db.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>自訂函式練習</title>
+    <style>
+        h1{
+            text-align: center;
+        }
+        .quiz{
+            width:100%;
+            padding:0.6rem 1rem;
+            border:blue;
+            background:lightblue;
+            box-shadow: 1px 1px 10px #ccc;
+            margin:0.5rem;
+        }
+    </style>
+</head>
+<body>
+ <h1>自訂函式練習</h1>
+ <div class="quiz">
+ 給定一個正整數的數值後，會印出對應行數的正三角形星星(依此類推可以設計印菱形，方形的函式)
+ </div>
+ <div class="quiz">
+ all()-給定資料表名後，會回傳整個資料表的資料
+ </div>
+ <div class="quiz">
+ find()-會回傳資料表指定id的資料
+ </div>
+ <div class="quiz">
+ update()-給定資料表的條件後，會去更新相應的資料。
+ </div>
+ <div class="quiz">
+ insert()-給定資料內容後，會去新增資料到資料表
+ </div>
+ <div class="quiz">
+ del()-給定條件後，會去刪除指定的資料
+ </div>
+ 
+
+</body>
+</html>
+<?php
+//給定一個正整數的數值後，會印出對應行數的正三角形星星(依此類推可以設計印菱形，方形的函式)
+function starts(){
+
+}
+
+//all()-給定資料表名後，會回傳整個資料表的資料
+function all(){
+
+}
+
+
+//find()-會回傳資料表指定id的資料
+function find(){
+
+}
+
+
+//update()-給定資料表的條件後，會去更新相應的資料。
+function update(){
+
+}
+
+
+//insert()-給定資料內容後，會去新增資料到資料表
+function insert(){
+
+}
+
+
+//del()-給定條件後，會去刪除指定的資料
+function del(){
+
+}
+
+?>
